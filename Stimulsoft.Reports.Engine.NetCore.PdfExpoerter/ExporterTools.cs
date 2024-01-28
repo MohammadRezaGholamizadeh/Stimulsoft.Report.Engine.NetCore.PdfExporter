a2299f25b8d821f7a0392b91a534a7f6835ea656
M.Gholamizadeh <pranceoffire50@gmail.com>
2024-01-29 02:17:25 +0330

    feat: => (implement the component of stimulsoft pdf exporter)

---
+++ Stimulsoft.Reports.Engine.NetCore.PdfExpoerter/ExporterTools.cs
@@ -0,0 +1,139 @@
+﻿using Stimulsoft.Base;
+using Stimulsoft.Report;
+using Stimulsoft.Report.Dictionary;
+using Stimulsoft.Report.Export;
+using System.Drawing;
+
+namespace Stimulsoft.Reports.Engine.NetCore.PdfExpoerter
+{
+    public class StiExporterTools
+    {
+        public StiExporterTools()
+        {
+            DataContents = new Dictionary<string, dynamic>();
+            Fonts = new List<byte[]>();
+        }
+
+        public Dictionary<string, dynamic> DataContents { get; private set; }
+        public List<byte[]> Fonts { get; private set; }
+        public byte[] MrtFileContent { get; set; }
+        public string StimulsoftLicenseKey { get; set; } = default;
+
+    }
+
+    public static class ExporterTools
+    {
+        public static StiExporterTools StartReport()
+        {
+            return new StiExporterTools();
+        }
+        public static StiExporterTools SetLicenseKey(
+            this StiExporterTools tools,  
+            string licenseKey)
+        {
+            StiLicense.LoadFromString(licenseKey);
+            return tools;
+        }
+
+        public static StiExporterTools AddFontBytes(
+            this StiExporterTools tools,
+            byte[] font,
+            string? alias = null,
+            FontStyle? fontStyle = null)
+        {
+            try
+            {
+                StiFontCollection
+                    .AddFontBytes(
+                        font,
+                        alias,
+                        fontStyle);
+
+                tools.Fonts.Add(font);
+            }
+            catch
+            {
+                throw;
+            }
+
+            return tools;
+        }
+
+        public static StiExporterTools AddDataContentAsBusinessObject(
+          this StiExporterTools tools,
+          string name,
+          dynamic data)
+        {
+            try
+            {
+                tools.DataContents.Add(name, data);
+            }
+            catch
+            {
+                throw;
+            }
+
+            return tools;
+        }
+
+        public static StiExporterTools SetMrtFile(
+            this StiExporterTools tools,
+            byte[] mrtFileBytes)
+        {
+            try
+            {
+                tools.MrtFileContent = mrtFileBytes;
+            }
+            catch
+            {
+                throw;
+            }
+
+            return tools;
+        }
+
+        public static StiReport RenderWithBusinessObjectDataMode(
+            this StiExporterTools tools,
+            bool showProgressState = false)
+        {
+            var report = new StiReport();
+            report.Load(tools.MrtFileContent);
+
+            report.RegBusinessObject(
+                tools.DataContents
+                     .Select(_ => new StiBusinessObjectData(null, _.Key, _.Value))
+                     .ToList());
+
+            report.Render(showProgressState);
+
+            return report;
+        }
+
+        public static void ExportPdfToPath(this StiReport report, string absolutePath)
+        {
+            report.ExportDocument(StiExportFormat.Pdf, absolutePath);
+        }
+
+        public static Stream ExportPdfAsMemoryStream(this StiReport report, StiExportSettings settings)
+        {
+            settings ??= new StiPdfExportSettings();
+
+            var memoryStream = new MemoryStream();
+            report.ExportDocument(StiExportFormat.Pdf, memoryStream, settings);
+            memoryStream.Seek(0, SeekOrigin.Begin);
+            return memoryStream;
+        }
+
+        public static Stream ExportPdfAsCustomStream(
+            this StiReport report,
+            Stream customStream,
+            StiExportSettings settings)
+        {
+            settings ??= new StiPdfExportSettings();
+
+            report.ExportDocument(StiExportFormat.Pdf, customStream, settings);
+            customStream.Seek(0, SeekOrigin.Begin);
+            return customStream;
+        }
+    }
+}
---
+++ StimulsoftPdfExporterSample/Program.cs
@@ -0,0 +1,25 @@
+﻿using Stimulsoft.Base;
+using Stimulsoft.Reports.Engine.NetCore.PdfExpoerter;
+
+
+
+var datas = new List<dynamic>()
+{
+    "متن اول به زبان فارسی",
+    "این متن برای تست زیرساخت استیمول سافت است",
+    "لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک است. چاپگرها و متون بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم است و برای شرایط فعلی تکنولوژی مورد نیاز و کاربردهای متنوع با هدف بهبود ابزارهای کاربردی می باشد. کتابهای زیادی در شصت و سه درصد گذشته، حال و آینده شناخت فراوان جامعه و متخصصان را می طلبد تا با نرم افزارها شناخت بیشتری را برای طراحان رایانه ای علی الخصوص طراحان خلاقی و فرهنگ پیشرو در زبان فارسی ایجاد کرد. در این صورت می توان امید داشت که تمام و دشواری موجود در ارائه راهکارها و شرایط سخت تایپ به پایان رسد و زمان مورد نیاز شامل حروفچینی دستاوردهای اصلی و جوابگوی سوالات پیوسته اهل دنیای موجود طراحی اساسا مورد استفاده قرار گیرد.",
+    "لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک است. چاپگرها و متون بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم است و برای شرایط فعلی تکنولوژی مورد نیاز و کاربردهای متنوع با هدف بهبود ابزارهای کاربردی می باشد. کتابهای زیادی در شصت و سه درصد گذشته، حال و آینده شناخت فراوان جامعه و متخصصان را می طلبد تا با نرم افزارها شناخت بیشتری را برای طراحان رایانه ای علی الخصوص طراحان خلاقی و فرهنگ پیشرو در زبان فارسی ایجاد کرد. در این صورت می توان امید داشت که تمام و دشواری موجود در ارائه راهکارها و شرایط سخت تایپ به پایان رسد و زمان مورد نیاز شامل حروفچینی دستاوردهای اصلی و جوابگوی سوالات پیوسته اهل دنیای موجود طراحی اساسا مورد استفاده قرار گیرد."
+};
+
+
+
+var fontName = "ANegaar";
+var fontBytes = File.ReadAllBytes(@$"D:\StimulsoftProject\Stimulsoft.Reports.Engine.NetCore.PdfExpoerter\StimulsoftPdfExporterSample\Fonts\{fontName}.ttf");
+var mrtFileContent = File.ReadAllBytes($@"D:\StimulsoftProject\Stimulsoft.Reports.Engine.NetCore.PdfExpoerter\StimulsoftPdfExporterSample\MrtFiles\Report.mrt");
+
+ExporterTools.StartReport()
+             .AddFontBytes(fontBytes)
+             .SetMrtFile(mrtFileContent)
+             .AddDataContentAsBusinessObject("Datas", datas)
+             .RenderWithBusinessObjectDataMode()
+             .ExportPdfToPath($@"C:\Users\Phoenix\Downloads\AllPdf\{fontName}.pdf");
